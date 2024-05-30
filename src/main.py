c464a76a321c66f0891b699f2cfa7b28a47fd444
Yaroslav Shahanov <72661508+yshahanov@users.noreply.github.com>
2024-05-30 18:18:45 +0200

    Add files via upload
    added viz

---
+++ src/main.py
@@ -0,0 +1,12 @@
+
+from visualizer import Visualizer
+
+if __name__ == '__main__':
+    cfb = "C:/Users/Yaroslav/PycharmProjects/PointCloud/SemanticKITTI-visualizer/src/config/visualizer.yaml"
+
+    visualizer = Visualizer(cfb)
+
+    visualizer.run()
+    
+    
+    
